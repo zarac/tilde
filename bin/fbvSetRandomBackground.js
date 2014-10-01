0f46c537a6f7d8afe46a5dbbf5addb54c55c5e23
Hannes Landstedt <git@zarac.se>
2014-10-02 01:52:24 +0200

    set background to some random picture on the filesystem. uses locate and fbv (frame buffer viewer).

---
+++ bin/fbvSetRandomBackground.js
@@ -0,0 +1,14 @@
+#!/bin/env node
+
+var exec = require('child_process').exec
+
+function randomFromTo(from, to) {
+  return Math.floor(Math.random() * (to - from + 1)) + from }
+
+exec('find /srv/images/* -type f', function(err, stdout, stderr) {
+  console.log('gotz', err, stdout, stderr)
+  var lines = stdout.split('\n')
+  var cmd = 'timeout 5 fbv -k "' + lines[randomFromTo(0, lines.length - 1)] + '"'
+  console.log('doing', cmd)
+  exec(cmd, function(err, stdout, stderr) {
+    console.log('done with', cmd, err, stdout, stderr) } ) } )
