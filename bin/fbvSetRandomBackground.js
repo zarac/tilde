e1170143e2852492dc4cd403fab490804e315891
Hannes Landstedt <git@zarac.se>
2015-01-25 22:46:11 +0100

    [change] fbv only utilize jpg,png,gif

--- bin/fbvSetRandomBackground.js
+++ bin/fbvSetRandomBackground.js
@@ -5,7 +5,7 @@ var exec = require('child_process').exec
 function randomFromTo(from, to) {
   return Math.floor(Math.random() * (to - from + 1)) + from }
 
-exec('find /srv/image/* -type f', function(err, stdout, stderr) {
+exec("find /srv/image/* -type f -regex '.*\\(jpg\\|png\\|gif\\)'", function(err, stdout, stderr) {
   console.log('gotz', err, stdout, stderr)
   var lines = stdout.split('\n')
   var cmd = 'timeout 5 fbv -k "' + lines[randomFromTo(0, lines.length - 1)] + '"'
