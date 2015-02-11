2cf76288689efab2794dcb011d2a826be9a84428
Hannes Landstedt <git@zarac.se>
2015-02-11 17:07:43 +0100

    delete remote files for tweek backup, also log.

--- bin/backitup-tweek-to-pip.sh
+++ bin/backitup-tweek-to-pip.sh
@@ -1,4 +1,3 @@
 #!/bin/env sh
 
-rsync -av --copy-unsafe-links ~/.config/backitup/ zarac.se:/srv/backup/tweek/
-#rsync -avL /srv/{git,home,http,image,music,slask,sound,video} zarac.se:/srv/backup/tweek/
+rsync -av --copy-unsafe-links --delete ~/.config/backitup/ zarac.se:/srv/backup/tweek/ | tee ~/var/log/backitup/`date +%Y%m%d%H%M`.log
